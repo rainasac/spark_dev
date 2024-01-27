52b27dc3093979c353493d2578c36abe8db8fcfe
Sachin <sachinraina503@gmail.com>
2024-01-27 09:01:24 +0530

    changes done

---
+++ src/main/scala/second.scala
@@ -0,0 +1,11 @@
+object second {
+  def main(args: Array[String]):Unit ={
+    print("Enter weight : ")
+    var a=scala.io.StdIn.readInt();
+     var b=a*1000;
+
+
+    print("The value of weight in grams : " +b)
+
+  }
+}
